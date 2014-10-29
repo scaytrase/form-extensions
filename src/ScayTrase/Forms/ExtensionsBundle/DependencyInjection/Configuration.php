6a2fdc22dbc9820e970f048615c38e865bfdb79d
Pavel Batanov <pavel@batanov.me>
2014-10-29 19:35:18 +0300

    Update configuration root

--- src/ScayTrase/Forms/ExtensionsBundle/DependencyInjection/Configuration.php
+++ src/ScayTrase/Forms/ExtensionsBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        // $rootNode = $treeBuilder->root('extensions');
+        $treeBuilder->root('scaytrase_form_extensions');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
