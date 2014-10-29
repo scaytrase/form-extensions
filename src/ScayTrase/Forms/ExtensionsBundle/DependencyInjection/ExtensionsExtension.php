f8096f8f722774c353438f8fbf5bff55aa1b0426
Pavel Batanov <pavel@batanov.me>
2014-10-29 19:40:30 +0300

    Remove unused variable

--- src/ScayTrase/Forms/ExtensionsBundle/DependencyInjection/ExtensionsExtension.php
+++ src/ScayTrase/Forms/ExtensionsBundle/DependencyInjection/ExtensionsExtension.php
@@ -21,7 +21,7 @@ class ExtensionsExtension extends Extension implements PrependExtensionInterface
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
